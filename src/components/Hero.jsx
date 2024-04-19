89ae8374c6aba8e506527b08200b25a24c6d7f15
bblackskulll063 <98683404+bblackskulll063@users.noreply.github.com>
2024-04-20 04:26:10 +0530

    add pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HERO_CONTENT } from '../constants/index';
-import profilePic from '../assets/kevinRushProfile.png'
+import profilePic from '../assets/him.jpg'
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
@@ -37,12 +37,13 @@ const Hero = () => {
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
-                    <div className="flex justify-center">
+                    <div className="flex justify-center ">
                         <motion.img
                         initial={{ x:100, opacity: 0}}
                         animate={{ x: 0, opacity: 1 }}
                         transition={{duration: 1, delay:1.2}}
-                        src={profilePic} alt="Pofile Pic" />
+                        src={profilePic} alt="Pofile Pic" 
+                        className='rounded-2xl '/>
                     </div>
                 </div>
             </div>
